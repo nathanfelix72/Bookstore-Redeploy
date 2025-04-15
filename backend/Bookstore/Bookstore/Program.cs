c17b12583a67c19181e80c15c28733a3295b49fd
Nathan Felix <nathan.felix@gmail.com>
2025-04-15 17:54:02 -0600

    cors

--- backend/Bookstore/Bookstore/Program.cs
+++ backend/Bookstore/Bookstore/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000, https://black-desert-09710141e.6.azurestaticapps.net")
                   .AllowCredentials()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
--- backend/Bookstore/Bookstore/obj/Debug/net8.0/Bookstore.AssemblyInfo.cs
+++ backend/Bookstore/Bookstore/obj/Debug/net8.0/Bookstore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b955012719891a5ba3627880df65d24dac5ecce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
